6884c6f07fb0b9c2f2202a26694467306bab888f
Diego <diegofernandez29400@gmail.com>
2019-03-04 23:02:31 +0100

    Realizo el main segun lo que me pide el enunciado

--- src/vehiculos/Main.java
+++ src/vehiculos/Main.java
@@ -20,7 +20,45 @@ public class Main {
         empresa.vehiculosAleatorios();
         
         //Mostramos por pantalla los datos aleatorios que hemos generado anteriormente
-        empresa.imprimirClientes1();
+        empresa.imprimirClientes();
         empresa.imprimirVehiculos();
+        
+         //Pedimos al usuario que introduzca su dni
+        System.out.println("Introduce su NIF");
+        String nif = teclado.nextLine();
+        
+        //Pedimos que introduzca la matricula
+        System.out.println("Introduce la matricula del vehiculo");
+        String matricula = teclado.nextLine();
+        
+        //Preguntamos el numero de dias del alquiler
+        System.out.println("Introduce el número de dias de alquiler");
+        int alquiler = teclado.nextInt();
+        
+        //Mediante el metodo alquilarVehiculo registramos el alquiler realizado por el usuario
+        empresa.alquilarVehiculo(matricula, nif, alquiler);
+        //He comentado esta parte del codigo ya que no encuentro el error
+        
+        //Ordenamos los clientes y los vehiculos
+        empresa.ordenarCarteraClientes();
+        empresa.ordenarCatalogoVehiculos();
+        
+        //Limpiamos el buffer
+        teclado.nextLine();
+        
+        //Pedimos otra vez el nif al usuario
+        System.out.println("Introduce de nuevo el nif");
+        nif = teclado.nextLine();
+        
+        //Buscamos al cliente y lo mostramos por pantalla
+        System.out.println(empresa.busquedaBinariaNif(nif));
+        
+        //Pedimos otra vez la matricula
+        System.out.println("Introduce la matricula");
+        matricula = teclado.nextLine();
+        
+        //Buscamos el vehiculo por la matricula y los mostramos por pantalla
+        System.out.println(empresa.busquedaBinariaMatricula(matricula));
+        
     }
 }
